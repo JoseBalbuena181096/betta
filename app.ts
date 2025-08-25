dd22e1309a0ca5f28e13f12495f8d59c2083b0eb
José Balbuena <45837292+JoseBalbuena181096@users.noreply.github.com>
2025-08-25 13:49:01 -0600

    second commit

--- app.ts
+++ app.ts
@@ -16,3 +16,8 @@ function getUser(id: number, users: User[]): User | undefined {
     return users.find(user => user.id === id)!;
 }
 
+/**
+ STAGING AREA
+ Es la parte intermedia entre tener los ficheros pero aun no hecho nada con ellos, en una carpeta temporal
+ si quiero registrar estos cambios guardar una foto de este proyecto, una version en el historial del proyecto
+ */
