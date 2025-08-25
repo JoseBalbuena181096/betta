faf746c2275f63f95de739258c84ca5863b41b53
José Balbuena <45837292+JoseBalbuena181096@users.noreply.github.com>
2025-08-25 13:46:25 -0600

    Initial commit

---
+++ app.ts
@@ -0,0 +1,18 @@
+console.log("Hello, TypeScript")
+
+const user = {
+    id:1,
+    name: "John",
+    email: "john@gmai.com"
+}
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+}
+
+function getUser(id: number, users: User[]): User | undefined {
+    return users.find(user => user.id === id)!;
+}
+
